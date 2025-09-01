cba6710df390649e08d49b1e3849af1b766d33d2
Vansh <131522589+Vansh27189@users.noreply.github.com>
2025-09-01 21:00:38 +0530

    Update CODE_DEBUGGER.py

--- CODE_DEBUGGER.py
+++ CODE_DEBUGGER.py
@@ -18,7 +18,7 @@ if hf_token is None:
 st.write("Loaded key starts with:", hf_token[:6])
 
 input1 = HuggingFaceEndpoint(
-    repo_id="openai/gpt-oss-120b",
+    repo_id="meta-llama/Llama-3.1-8B-Instruct",
     # task="text-generator",
     api_token=hf_token,
     model_kwargs={
@@ -138,6 +138,7 @@ if st.button("Analyse"):
     result = model.invoke(prompt)
     st.write("Result: ")
     st.write(result.content)
+
 
 
 
